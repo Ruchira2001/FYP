0605a2171272a162d08c520ff1ac0eb16acb75b2
Ruchira Ilangasinha <ruchirailangasinha2001@gmail.com>
2026-01-15 01:19:15 +0530

    Delete admin directory

--- admin/src/components/index.js
+++
@@ -1,6 +0,0 @@
-// Component Exports for Goviconnect SL Admin Dashboard
-
-export { default as Sidebar } from './Sidebar';
-export { default as Header } from './Header';
-export { default as StatCard } from './StatCard';
-export { LineChart, BarChart, DoughnutChart } from './Charts';
