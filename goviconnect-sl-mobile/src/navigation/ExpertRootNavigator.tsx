234b09c6bc8f67f07c62e40c56f289f9b5eaff8d
ruchira2001 <ruchirailangasinha2001@gmail.com>
2026-02-28 20:03:55 +0530

    Implement ExpertRootNavigator to manage expert authentication and main application navigation flows.

--- goviconnect-sl-mobile/src/navigation/ExpertRootNavigator.tsx
+++ goviconnect-sl-mobile/src/navigation/ExpertRootNavigator.tsx
@@ -7,6 +7,7 @@ import ExpertTabNavigator from './ExpertTabNavigator';
 
 // Screens
 import ExpertChatDetail from '../screens/expert/chats/ExpertChatDetail';
+import ExpertChatsList from '../screens/expert/chats/ExpertChatsList';
 import FarmerDirectory from '../screens/expert/farmers/FarmerDirectory';
 import FarmerRequests from '../screens/expert/requests/FarmerRequests';
 import ExpertKnowledgeBase from '../screens/expert/knowledge/ExpertKnowledgeBase';
@@ -32,6 +33,7 @@ const MainNavigator = () => (
 
         {/* Global screens accessible from anywhere */}
         <RootStack.Screen name="ExpertChatDetail" component={ExpertChatDetail} />
+        <RootStack.Screen name="ExpertChatsList" component={ExpertChatsList} />
         <RootStack.Screen name="ExpertFarmerDirectory" component={FarmerDirectory} />
         <RootStack.Screen name="ExpertFarmerRequests" component={FarmerRequests} />
         <RootStack.Screen name="ExpertKnowledgeBase" component={ExpertKnowledgeBase} />
